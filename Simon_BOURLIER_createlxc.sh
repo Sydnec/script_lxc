89169302206ede7071f5d4c18df2c3d0f261f0bb
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-11-23 23:04:04 +0100

    d

--- Simon_BOURLIER_createlxc.sh
+++ Simon_BOURLIER_createlxc.sh
@@ -84,22 +84,22 @@ while getopts "n:d:r:a:u:h" opt; do
         exit 0
         ;;
     \?)
-        error "Utilisation: $myself"
+        error "Utilisation: $myself" 1
         ;;
     esac
 done
 
 if [ -z "$(dpkg -l | grep -w 'lxc') | grep -w "lxc")" ]; then
     info "Installing lxc"
-    sudo apt install -qq lxc >/dev/null 2>&1 && success "lxc intalled" || error "lxc to settle in"
+    sudo apt install -qq lxc >/dev/null 2>&1 && success "lxc intalled" || error "lxc to settle in" 1
 fi
 if [ -z "$(grep '^lxc\.net\.0\.hwaddr.*xx:xx:xx$' /etc/lxc/default.conf)" ]; then
     info "Editing /etc/lxc/default.conf"
-    sudo sed -i '/lxc.net.0.flags = up/a lxc.net.0.hwaddr = 00:16:3e:xx:xx:xx' /etc/lxc/default.conf >/dev/null 2>&1 && success "/etc/lxc/default.conf edited" || error "fail to edit /etc/lxc/default.conf"
+    sudo sed -i '/lxc.net.0.flags = up/a lxc.net.0.hwaddr = 00:16:3e:xx:xx:xx' /etc/lxc/default.conf >/dev/null 2>&1 && success "/etc/lxc/default.conf edited" || error "fail to edit /etc/lxc/default.conf" 1
 fi
 
-sudo lxc-create -t download -n $lxc_name -- -d $distr_name -r $release -a $arch >/dev/null 2>&1 && success "Container created" || error "lxc container failed to create"
-sudo lxc-start -n $lxc_name && success "Container launched" || error "lxc container failed to launch"
+sudo lxc-create -t download -n $lxc_name -- -d $distr_name -r $release -a $arch >/dev/null 2>&1 && success "Container created" || error "lxc container failed to create" 1
+sudo lxc-start -n $lxc_name && success "Container launched" || error "lxc container failed to launch" 1
 
 info "Waiting internet connection"
 while ! sudo lxc-attach -n $lxc_name -- ping -c 1 8.8.8.8 >/dev/null 2>&1; do
@@ -110,7 +110,7 @@ success "Internet connection etablished"
 sudo lxc-attach -n $lxc_name -- bash -c '
   echo "fr_FR.UTF-8 UTF-8" > /etc/locale.gen &&
   locale-gen &&
-  update-locale LANG=fr_FR.UTF-8' >/dev/null 2>&1 && success "Keybord setted up" || error "Failed to set up keyboard"
+  update-locale LANG=fr_FR.UTF-8' >/dev/null 2>&1 && success "Keybord setted up" || error "Failed to set up keyboard" 1
 
 sudo lxc-attach -n $lxc_name -- bash -c '
   apt update &&
@@ -118,7 +118,7 @@ sudo lxc-attach -n $lxc_name -- bash -c '
 
 sudo lxc-attach -n $lxc_name -- bash -c '
   useradd '"$username"' &&
-  echo "'"$username"':'"$passwd"'" | chpasswd' >/dev/null 2>&1 && success "User $username added" || error "Failed to add user $username"
+  echo "'"$username"':'"$passwd"'" | chpasswd' >/dev/null 2>&1 && success "User $username added" || error "Failed to add user $username" 1
 
 container_ip=$(sudo lxc-info -n $lxc_name | awk '/IP:/ {print $2}')
 cat <<-EOF
