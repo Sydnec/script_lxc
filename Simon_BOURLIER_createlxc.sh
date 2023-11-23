7127ea81a31208d5d25194c626b3d8932bbefd4c
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-11-23 18:37:11 +0100

    Rename file

--- Simon_BOURLIER_createlxc.sh
+++ Simon_BOURLIER_createlxc.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 myself=$(basename "$0") # Nom du script
 
-sudo apt install lxc
-sudo vi /etc/lxc/default.conf
 sudo lxc-create -t download -n c1 -- -d debian -r bullseye -a amd64
 sudo lxc-start -n c1
 sudo lxc-attach -n c1
