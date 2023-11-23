b7e8c4c5ade4f30bd84c6aa2ac949b0cd9fbd1af
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-11-23 21:56:25 +0100

    d

--- Simon_BOURLIER_createlxc.sh
+++ Simon_BOURLIER_createlxc.sh
@@ -92,6 +92,11 @@ done
 [ -z "$(dpkg -l | grep -w 'lxc') | grep -w "lxc")" ] && sudo apt install -qq lxc
 [ -z "$(grep '^lxc\.net\.0\.hwaddr.*xx:xx:xx$' /etc/lxc/default.conf)" ] && sudo sed -i '/lxc.net.0.flags = up/a lxc.net.0.hwaddr = 00:16:3e:xx:xx:xx' /etc/lxc/default.conf
 
+success $lxc_name
+success $distr_name
+success $release
+success $arch
+
 sudo lxc-create -t download -n $lxc_name -- -d $distr_name -r $release -a $arch || error "Erreur lors de la création du conteneur lxc"
 sudo lxc-start -n $lxc_name || error "Erreur lors du lancement du conteneur lxc"
 
