760826b87598c263fe5474e624f2a5ead26b2e5b
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-11-23 22:52:28 +0100

    d

--- Simon_BOURLIER_createlxc.sh
+++ Simon_BOURLIER_createlxc.sh
@@ -136,7 +136,7 @@ EOF
 if [ "$auto_connect" == false ]; then
     sudo lxc-ls -f
 else
-    ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/key_$lxc_name >/dev/null 2>&1 && success "Generated key" || error "Key generation"
+    sudo ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/key_$lxc_name >/dev/null 2>&1 && success "Generated key" || error "Key generation"
     sudo ssh-copy-id -i ~/.ssh/key_$lxc_name $username@$container_ip 2>&1 && success "Keys copied" || error "Key copy"
     ssh $username@$container_ip
 fi
