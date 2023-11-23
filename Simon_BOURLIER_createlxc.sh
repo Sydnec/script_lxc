cf2990fe529d0c4b44060b957a51a2dc7f1dccf2
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-11-23 20:05:31 +0100

    d

--- Simon_BOURLIER_createlxc.sh
+++ Simon_BOURLIER_createlxc.sh
@@ -4,19 +4,13 @@ set -euo pipefail
 
 myself=$(basename "$0") # Nom du script
 
-if [ -z $(apt list --installed 2>/dev/null | grep -w "lxc") ]; then 
-    sudo apt install -qq lxc;
-fi
-if [ -z $(grep '^lxc\.net\.0\.hwaddr.*xx:xx:xx$' /etc/lxc/default.conf) ]; then
-    sudo sed -i '/lxc.net.0.flags = up/a lxc.net.0.hwaddr = 00:16:3e:xx:xx:xx' /etc/lxc/default.conf;
-fi
-
-
-# sudo lxc-create -t download -n c1 -- -d debian -r bullseye -a amd64
-# sudo lxc-start -n c1
-# sudo lxc-attach -n c1
-# dpkg-reconfigure locales
-# apt update
-# apt install ssh sudo
-# adduser user
-# adduser user sudo
+[ -z "$(dpkg -l | grep -w 'lxc') | grep -w "lxc")" ] && sudo apt install -qq lxc 
+[ -z "$(grep '^lxc\.net\.0\.hwaddr.*xx:xx:xx$' /etc/lxc/default.conf)" ] && sudo sed -i '/lxc.net.0.flags = up/a lxc.net.0.hwaddr = 00:16:3e:xx:xx:xx' /etc/lxc/default.conf
+sudo lxc-create -t download -n c1 -- -d debian -r bullseye -a amd64
+sudo lxc-start -n c1
+sudo lxc-attach -n c1
+dpkg-reconfigure locales
+apt update
+apt install ssh sudo
+adduser user
+adduser user sudo
