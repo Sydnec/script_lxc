e099be63ae9886cb1225e75201070d2b43d277a8
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-11-23 19:00:47 +0100

    Test si lxc est installé

--- Simon_BOURLIER_createlxc.sh
+++ Simon_BOURLIER_createlxc.sh
@@ -4,11 +4,17 @@ set -euo pipefail
 
 myself=$(basename "$0") # Nom du script
 
-sudo lxc-create -t download -n c1 -- -d debian -r bullseye -a amd64
-sudo lxc-start -n c1
-sudo lxc-attach -n c1
-dpkg-reconfigure locales
-apt update
-apt install ssh sudo
-adduser user
-adduser user sudo
+if dpkg-query -l lxc &> /dev/null; then
+    echo "oui"
+else
+    sudo apt install lxc
+    echo "non"
+fi
+# sudo lxc-create -t download -n c1 -- -d debian -r bullseye -a amd64
+# sudo lxc-start -n c1
+# sudo lxc-attach -n c1
+# dpkg-reconfigure locales
+# apt update
+# apt install ssh sudo
+# adduser user
+# adduser user sudo
