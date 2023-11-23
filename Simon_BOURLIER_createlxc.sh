33f6cf3d194cf02196c78ea787b5fb562dd02bde
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-11-23 19:12:57 +0100

    flemme

--- Simon_BOURLIER_createlxc.sh
+++ Simon_BOURLIER_createlxc.sh
@@ -4,12 +4,8 @@ set -euo pipefail
 
 myself=$(basename "$0") # Nom du script
 
-if dpkg-query -l lxc &> /dev/null; then
-    echo "oui"
-else
-    sudo apt install lxc
-    echo "non"
-fi
+[ ! dpkg-query -l lxc &> /dev/null; ] && sudo -qq apt install lxc
+
 # sudo lxc-create -t download -n c1 -- -d debian -r bullseye -a amd64
 # sudo lxc-start -n c1
 # sudo lxc-attach -n c1
