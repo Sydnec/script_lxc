e3d0c1fe18af46d589bc3cd15c6c2cf95d818153
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-11-23 20:10:07 +0100

    d

--- Simon_BOURLIER_createlxc.sh
+++ Simon_BOURLIER_createlxc.sh
@@ -8,9 +8,10 @@ myself=$(basename "$0") # Nom du script
 [ -z "$(grep '^lxc\.net\.0\.hwaddr.*xx:xx:xx$' /etc/lxc/default.conf)" ] && sudo sed -i '/lxc.net.0.flags = up/a lxc.net.0.hwaddr = 00:16:3e:xx:xx:xx' /etc/lxc/default.conf
 sudo lxc-create -t download -n c1 -- -d debian -r bullseye -a amd64
 sudo lxc-start -n c1
-sudo lxc-attach -n c1
-dpkg-reconfigure locales
-apt update
-apt install ssh sudo
-adduser user
-adduser user sudo
+sudo lxc-attach -n c1 -- bash -c '
+  dpkg-reconfigure locales
+  apt update
+  apt install -y ssh sudo
+  adduser user
+  adduser user sudo
+'
